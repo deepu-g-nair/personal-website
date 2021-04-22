ce33decf97c63a484f5f8ac0adbb97b61ffc83ee
Deepu G Nair <deepukrishna811@gmail.com>
2021-04-22 13:29:45 +0530

    about page added

--- src/App.js
+++ src/App.js
@@ -1,31 +1,13 @@
 import React from "react";
 import Home from "./components/Home";
 import "./app.scss";
+import About from "./components/About";
 
 const App = () => {
   return (
     <div>
       <Home />
-      <h1>qqqqqqqqqqqqqq</h1>
-      <h1>qqqqqqqqqqqqqq</h1>
-      <h1>qqqqqqqqqqqqqq</h1>
-      <h1>qqqqqqqqqqqqqq</h1>
-      <h1>qqqqqqqqqqqqqq</h1>
-      <h1>qqqqqqqqqqqqqq</h1>
-      <h1>qqqqqqqqqqqqqq</h1>
-      <h1>qqqqqqqqqqqqqq</h1>
-      <h1>qqqqqqqqqqqqqq</h1>
-      <h1>qqqqqqqqqqqqqq</h1>
-      <h1>qqqqqqqqqqqqqq</h1>
-      <h1>qqqqqqqqqqqqqq</h1>
-      <h1>qqqqqqqqqqqqqq</h1>
-      <h1>qqqqqqqqqqqqqq</h1>
-      <h1>qqqqqqqqqqqqqq</h1>
-      <h1>qqqqqqqqqqqqqq</h1>
-      <h1>qqqqqqqqqqqqqq</h1>
-      <h1>qqqqqqqqqqqqqq</h1>
-      <h1>qqqqqqqqqqqqqq</h1>
-      <h1>qqqqqqqqqqqqqq</h1>
+      <About />
     </div>
   );
 };
---
+++ src/components/About.js
@@ -0,0 +1,33 @@
+import React from "react";
+import AboutImage from "../assets/about.png";
+import "../styles/about.scss";
+
+const About = () => {
+  return (
+    <div className="about">
+      <div className="about-title">
+        <p className="about-main-title">ABOUT</p>
+        {/* <p className="about-sub-title">WHAT I AM</p> */}
+      </div>
+      <div className="about-body">
+        <div className="about-body-left">
+          <img src={AboutImage} alt="" />
+        </div>
+        <div className="about-body-right">
+          <p>
+            My name is Deepu G Nair. I am a passionate front-end web developer.
+            For the past one year, I am focusing more on this domain. Building
+            attractive and user-friendly interfaces are always my top priority.
+            Learning new techs in web development always brings me happiness and
+            actively looking for more. I have done couple of works and have
+            participated in a hackathon named 'Reboot Kerala Hackathon 2020'.
+            <br />
+            <button>Get my resume</button>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
